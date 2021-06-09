c0f89c2aa6456a8e2c890d21387ea7d0ef9b40af
Naveen Chalicheemala <naveen.chalicheemala@macys.com>
2021-06-09 18:13:46 -0400

    Updated settings.py

--- takeHome/settings.py
+++ takeHome/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ky$=k=xs8@e-nj4$x-i2+0cy2zhbw2@z)_pid#238h8%57j0kc'
+#SECRET_KEY = 'django-insecure-ky$=k=xs8@e-nj4$x-i2+0cy2zhbw2@z)_pid#238h8%57j0kc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['finddatatypeapp.herokuapp.com']
 
 
 # Application definition
