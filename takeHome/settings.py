62e3e1dcfa9f898e97bb6c2d5c0438aecdd2bffc
Naveen Chalicheemala <naveen.chalicheemala@macys.com>
2021-06-09 18:22:24 -0400

    Update requirements and added django-heroku

--- takeHome/settings.py
+++ takeHome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django_heroku.settings(locals())
